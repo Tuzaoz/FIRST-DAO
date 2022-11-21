e9c5a2d2feed8922d27d1f2aa925a2cf1f6adc22
Arthur Dantas <arthurdantaspro@gmail.com>
2022-11-21 20:04:57 -0300

    Criado classe Seller

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -1,6 +1,88 @@
 package model.entities;
 
-public class Seller {
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Seller implements Serializable {
     private Integer id;
-    private String
+    private String name;
+    private String emai;
+    private LocalDate birthDate;
+    private Double baseSalary;
+    private Department department;
+
+    public Seller(Integer id) {
+        this.id = id;
+    }
+
+    public Seller(Integer id, String name, String emai, LocalDate birthDate, Double baseSalary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.emai = emai;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmai() {
+        return emai;
+    }
+
+    public void setEmai(String emai) {
+        this.emai = emai;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return Objects.equals(id, seller.id) && Objects.equals(name, seller.name) && Objects.equals(emai, seller.emai) && Objects.equals(birthDate, seller.birthDate) && Objects.equals(baseSalary, seller.baseSalary) && Objects.equals(department, seller.department);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, emai, birthDate, baseSalary, department);
+    }
 }
